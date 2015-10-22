c8c38b553fd1928c1027b3d2861e203fd99bcff8
nick0916 <nickliao2013@gmail.com>
2015-10-22 13:25:43 +0800

    update new version

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
 
-    setInterval(function(){
-      $.ajax({
-            method: "POST",
-            url: "do.php"
-        }).done(function(data){
-            $("#board").html(data);
-        })
-    }, 100);
+    
+	
 
-    $('#send').click(function(){
+    $('button').click(function(){
         $.ajax({
             method: "POST",
             url: "do.php",
             data: {name: $('#name').val(),message: $('textarea#message').val()}
         }).done(function(data){
+			
             $("#board").html(data);
-            $('input').val("");
+            //$('input').val("");
         });
     });
+	setInterval(function(){
+      $.ajax({
+            method: "POST",
+            url: "show.php"
+        }).done(function(data){
+            $("#board").html(data);
+        })
+    }, 100);
+    /*$(document).keydown(function(e){
+       if(e.keyCode == 13) {
+           $.ajax({
+                method: "POST",
+                url: "do.php",
+                data: {name: $('#name').val(),message: $('#message').val()}}
+            }).done(function(data){
+                $("#board").html(data);
+                $('input').val("");
+            });
+       }
+    });*/
 });
-
